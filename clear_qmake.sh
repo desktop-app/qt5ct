6e5daf78bee98b4bfcf83e04eb1e50dfb3949ad3
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2015-04-02 20:15:22 +0000

    fixed clear_qmake.sh script
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@139 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- clear_qmake.sh
+++ clear_qmake.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-###################################################
-# Removes remaining qmake files from qmmp package #
-###################################################
+####################################################
+# Removes remaining qmake files from qt5ct package #
+####################################################
 
 make distclean
-rm -rf lib
 find . -type f -name *.qm -delete
 find . -type d -name ".build" | xargs rm -rf
-rm -rf src/qmmpui/pkgconfig
-rm -rf src/qmmp/pkgconfig
