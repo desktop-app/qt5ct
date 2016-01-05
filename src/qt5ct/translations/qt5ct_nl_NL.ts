3c9c422379fb39f5323c7f65888a71903955eaa2
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2016-01-05 08:22:29 +0000

    updated Dutch (Netherlands) translation (Heimen Stoffels)
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@267 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct/translations/qt5ct_nl_NL.ts
+++ src/qt5ct/translations/qt5ct_nl_NL.ts
@@ -340,7 +340,7 @@
     <message>
         <location filename="../interfacepage.ui" line="209"/>
         <source>Activate item on single-click</source>
-        <translation type="unfinished"></translation>
+        <translation>Item activeren met enkele klik</translation>
     </message>
 </context>
 <context>
