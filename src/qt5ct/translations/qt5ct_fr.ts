b66f8165320d3e6f393d53713dd53905535f8c3e
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2016-09-01 11:55:45 +0000

    updated French translation (Geiger David)
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@322 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct/translations/qt5ct_fr.ts
+++ src/qt5ct/translations/qt5ct_fr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
 <context>
     <name>AppearancePage</name>
     <message>
@@ -653,17 +651,17 @@
     <message>
         <location filename="../main.cpp" line="60"/>
         <source>Please remove the &lt;b&gt;QT_STYLE_OVERRIDE&lt;/b&gt; environment variable</source>
-        <translation type="unfinished"></translation>
+        <translation>Merci de retirer la variable d&apos;environement &lt;b&gt;QT_STYLE_OVERRIDE&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="65"/>
         <source>The &lt;b&gt;QT_QPA_PLATFORMTHEME&lt;/b&gt; environment variable is not set correctly</source>
-        <translation type="unfinished"></translation>
+        <translation>La variable d&apos;environnement &lt;b&gt;QT_QPA_PLATFORMTHEME&lt;/b&gt; n&apos;est pas définie correctement</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="71"/>
         <source>Error</source>
-        <translation type="unfinished">Erreur</translation>
+        <translation>Erreur</translation>
     </message>
 </context>
 </TS>
