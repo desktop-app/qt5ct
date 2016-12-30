12281c90ba0f9054253c904a3a58c8d60940b5ee
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2016-12-30 07:30:52 +0000

    updated copyrights
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@341 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct-qtplugin/qt5ctproxystyle.h
+++ src/qt5ct-qtplugin/qt5ctproxystyle.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2016, Ilya Kotov <forkotov02@hotmail.ru>
+ * Copyright (c) 2014-2017, Ilya Kotov <forkotov02@hotmail.ru>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
