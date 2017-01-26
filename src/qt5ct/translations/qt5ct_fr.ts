509d28b1f0088e8070baa2f7e83794704358a070
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2017-01-26 05:18:41 +0000

    updated French translation (Geiger David)
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@368 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct/translations/qt5ct_fr.ts
+++ src/qt5ct/translations/qt5ct_fr.ts
@@ -663,7 +663,7 @@
     <message>
         <location filename="../main.cpp" line="71"/>
         <source>Unable to find &lt;b&gt;libqt5ct-style.so&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de trouver &lt;b&gt;libqt5ct-style.so&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="76"/>
