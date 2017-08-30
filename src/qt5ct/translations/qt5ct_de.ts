60f6f63e3840b9eb35d9f56902d6d51e3a1ae9f6
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2017-08-30 06:55:06 +0000

    updated German translation (Ettore Atalan)
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@428 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct/translations/qt5ct_de.ts
+++ src/qt5ct/translations/qt5ct_de.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
 <context>
     <name>AppearancePage</name>
     <message>
@@ -355,7 +353,7 @@
     <message>
         <location filename="../interfacepage.ui" line="246"/>
         <source>Show shortcut underlines</source>
-        <translation type="unfinished"></translation>
+        <translation>Verknüpfungsunterstriche anzeigen</translation>
     </message>
     <message>
         <location filename="../interfacepage.cpp" line="47"/>
