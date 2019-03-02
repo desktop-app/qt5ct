a84ed17357aa7c04412b4973f18f8d1f9edc7883
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2019-03-02 09:38:00 +0000

    updated Russian translation
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@520 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- src/qt5ct/translations/qt5ct_ru.ts
+++ src/qt5ct/translations/qt5ct_ru.ts
@@ -342,7 +342,7 @@
     <message>
         <location filename="../interfacepage.ui" line="192"/>
         <source>Keyboard scheme:</source>
-        <translation type="unfinished"></translation>
+        <translation>Стандартные горячие клавиши:</translation>
     </message>
     <message>
         <location filename="../interfacepage.ui" line="202"/>
