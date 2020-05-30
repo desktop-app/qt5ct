449e6818f4016ff6aea0ee4faeb0fff09fcfc394
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2020-05-30 16:50:15 +0000

    fixed test.sh
    git-svn-id: https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct@569 8589a5e3-d8d1-45fb-8159-d1d5a4127d30

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ MYDIR=$(dirname $0)
 
 mkdir -p ${MYDIR}/src/qt5ct/styles
 ln -s "../../qt5ct-style/libqt5ct-style.so" "${MYDIR}/src/qt5ct/styles/"
-export QT_DEBUG_PLUGINS=1
+#export QT_DEBUG_PLUGINS=1
+export QT_LOGGING_RULES="qt5ct.debug=true"
 export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_QPA_PLATFORM_PLUGIN_PATH=${MYDIR}/src/qt5ct-qtplugin
 ${MYDIR}/src/qt5ct/qt5ct
